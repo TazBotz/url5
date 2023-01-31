fd55ba0a59af60df598d7d5a7310754ee3b7313d
TazBotz <115342407+TazBotz@users.noreply.github.com>
2023-01-31 22:14:02 +0530

    Update commands.py

--- CYNITE/commands.py
+++ CYNITE/commands.py
@@ -22,7 +22,7 @@ BATCH_FILES = {}
 async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [[
-                    InlineKeyboardButton('➕Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                   ],[
                     InlineKeyboardButton('Uᴘᴅᴀᴛᴇs', url='https://t.me/TazHD'),
                     InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ', url='https://t.me/XKunalBot')
@@ -45,13 +45,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-                    InlineKeyboardButton('➕Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                   ],[
-                    InlineKeyboardButton('ℹ️Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('😊Aʙᴏᴜᴛ', callback_data='about'),
+                    InlineKeyboardButton('ℹ️ Hᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('😊 Aʙᴏᴜᴛ', callback_data='about'),
                   ],[
-                    InlineKeyboardButton('🔍Sᴇᴀʀᴄʜ', url="https://t.me/+AJCnlql9y4o4MjJl"),
-                    InlineKeyboardButton('🤖Uᴘᴅᴀᴛᴇꜱ', url="https:t.me/TazHD")
+                    InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url="https://t.me/+AJCnlql9y4o4MjJl"),
+                    InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇꜱ', url="https:t.me/TazHD")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -93,13 +93,13 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-                    InlineKeyboardButton('➕Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+                    InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘs ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                   ],[
-                    InlineKeyboardButton('ℹ️Hᴇʟᴘ', callback_data='help'),
-                    InlineKeyboardButton('😊Aʙᴏᴜᴛ', callback_data='about'),
+                    InlineKeyboardButton('ℹ️ Hᴇʟᴘ', callback_data='help'),
+                    InlineKeyboardButton('😊 Aʙᴏᴜᴛ', callback_data='about'),
                   ],[
-                    InlineKeyboardButton('🔍Sᴇᴀʀᴄʜ', url="https://t.me/+AJCnlql9y4o4MjJl"),
-                    InlineKeyboardButton('🤖Uᴘᴅᴀᴛᴇꜱ', url="https://t.me/TazHD")
+                    InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url="https://t.me/+AJCnlql9y4o4MjJl"),
+                    InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇꜱ', url="https://t.me/TazHD")
                   ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
