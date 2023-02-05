6b57387c0d439f84c9525c5f05879b174b5ee0a4
TazBotz <115342407+TazBotz@users.noreply.github.com>
2023-02-05 07:49:50 +0530

    Update pm_filter.py

--- CYNITE/pm_filter.py
+++ CYNITE/pm_filter.py
@@ -633,10 +633,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         if query.from_user.id in ADMINS:
-            await client.edit_message_media(
-                query.message.chat.id,
-                query.message.id
-            )
             await query.message.edit_text(
                 text=script.ADMIN_TXT,
                 reply_markup=reply_markup,
