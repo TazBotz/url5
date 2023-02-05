e1cac661357c4e036bac5e784e49ad3191a442cd
TazBotz <115342407+TazBotz@users.noreply.github.com>
2023-02-05 20:43:31 +0530

    Update commands.py

--- CYNITE/commands.py
+++ CYNITE/commands.py
@@ -47,11 +47,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url='https://t.me/+AJCnlql9y4o4MjJl'),
-            InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇꜱ', url='https://t.me/TazHD')
-            ],[
             InlineKeyboardButton('ℹ️ Hᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('😊 Aʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🧑🏻‍💻 Aʙᴏᴜᴛ', callback_data='about')
+            ],[
+            InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url='https://t.me/+AJCnlql9y4o4MjJl'),
+            InlineKeyboardButton('📡 Uᴘᴅᴀᴛᴇꜱ', url='https://t.me/TazHD')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -95,11 +95,11 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('➕ Aᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘꜱ ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url='https://t.me/+AJCnlql9y4o4MjJl'),
-            InlineKeyboardButton('🤖 Uᴘᴅᴀᴛᴇꜱ', url='https://t.me/TazHD')
-            ],[
             InlineKeyboardButton('ℹ️ Hᴇʟᴘ', callback_data='help'),
-            InlineKeyboardButton('😊 Aʙᴏᴜᴛ', callback_data='about')
+            InlineKeyboardButton('🧑🏻‍💻 Aʙᴏᴜᴛ', callback_data='about')
+            ],[
+            InlineKeyboardButton('🔍 Sᴇᴀʀᴄʜ', url='https://t.me/+AJCnlql9y4o4MjJl'),
+            InlineKeyboardButton('📡 Uᴘᴅᴀᴛᴇꜱ', url='https://t.me/TazHD')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
