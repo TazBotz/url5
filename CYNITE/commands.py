34dbaeab4aa732c9aac65feb08c19051e2fb32b4
TazBotz <115342407+TazBotz@users.noreply.github.com>
2023-02-06 22:06:04 +0530

    Update commands.py

--- CYNITE/commands.py
+++ CYNITE/commands.py
@@ -84,7 +84,7 @@ async def start(client, message):
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton("↻ Tʀʏ Aɢᴀɪɴ", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")])
         await client.send_photo(
-            photo="https://telegra.ph/file/a4c2c5d8a999b47970227.jpg",
+            photo="https://telegra.ph/file/09fb3c91e89a98674afec.jpg",
             chat_id=message.from_user.id,
             caption=(script.FORCE_SUB),
             reply_markup=InlineKeyboardMarkup(btn),
