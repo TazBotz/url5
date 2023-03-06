874dc2dff67bf9b7ca7bf0a08314cba9da7fd4c3
TazBotz <115342407+TazBotz@users.noreply.github.com>
2023-03-06 14:50:18 +0530

    Update pm_filter.py

--- CYNITE/pm_filter.py
+++ CYNITE/pm_filter.py
@@ -27,6 +27,9 @@ from database.gfilters_mdb import (
 )
 import logging
 
+import os
+req_channel = int(os.environ.get('REQ_CHANNEL', '-1001614656948'))
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.ERROR)
 
@@ -931,6 +934,7 @@ async def auto_filter(client, msg, spoll=False):
             search = message.text
             files, offset, total_results = await get_search_results(search.lower(), offset=0, filter=True)
             if not files:
+                await client.send_message(req_channel,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝Content Name :{search}\nRequested By: {message.from_user.first_name}\n USER ID:{message.from_user.id}\n\n🗃", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Mark as Done 🔺", callback_data="close_data")]]))
                 if settings["spell_check"]:
                     return await advantage_spell_chok(msg)
                 else:
